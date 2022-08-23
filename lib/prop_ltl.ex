6380d33e756a89f860cd97bdb813b6d1b67610b7
Guilherme Grochau Azzi <gui.g.azzi@gmail.com>
2022-08-23 19:44:59 +0200

    Add first test stepping a proposition

--- lib/prop_ltl.ex
+++ lib/prop_ltl.ex
@@ -1,3 +1,20 @@
 defmodule PropLTL do
+  alias PropLTL.Proposition, as: Proposition
 
+  def init_simulation(module, init_arg, property) do
+    {:ok, state} = module.init(init_arg)
+
+    {state, step_property(state, {Proposition.simplify(property), %{state: state}})}
+  end
+
+  def step_simulation(module, event, state, prop) do
+    {:noreply, state} = module.handle_cast(event, state)
+    {state, step_property(state, prop)}
+  end
+
+  defp step_property(state, {property, env}) do
+    {property, env} = Proposition.step(Proposition.unfold(property), %{env | state: state})
+    property = Proposition.simplify(property)
+    {property, env}
+  end
 end
--- test/prop_ltl_test.exs
+++ test/prop_ltl_test.exs
@@ -1,5 +1,62 @@
 defmodule PropLTLTest do
   use ExUnit.Case
+  import PropLTL.Proposition
   import PropLTL
   doctest PropLTL
+
+  defmodule OnOff do
+    use GenServer
+
+    @impl true
+    def init(observer) do
+      {:ok, %{observer: observer, on?: false}}
+    end
+
+    @impl true
+    def handle_cast(event, state) do
+      new_state = handle_event(event, state)
+      send_effects(state, new_state)
+      {:noreply, new_state}
+    end
+
+    defp handle_event(:toggle, state) do
+      %{state | on?: not state.on?}
+    end
+
+    defp handle_event(:on, state) do
+      %{state | on?: true}
+    end
+
+    defp handle_event(:off, state) do
+      %{state | on?: false}
+    end
+
+    defp send_effects(curr, next) do
+      if next.on? != curr.on? do
+        send(next.observer, {:set_lights, next.on?})
+      end
+    end
+  end
+
+  describe "OnOff" do
+    test "try some steps" do
+      property =
+        prop do
+          always do
+            state.on? -- eventually(not state.on?)
+          end
+        end
+
+      property =
+        prop do
+          eventually(state.on?)
+        end
+
+      {state, {p, env}} = init_simulation(OnOff, self(), property)
+      assert p == property
+
+      {state, {p, env}} = step_simulation(OnOff, :toggle, state, {p, env})
+      assert p == true
+    end
+  end
 end
